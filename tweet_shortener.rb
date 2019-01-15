6202d566c72c500298d40a1ba64b8e6f476e6b99
LaRaye Johnson <lnj2105@gmail.com>
2019-01-15 18:43:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+
+
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be"=> "b",
+    "you"=> "u",
+    "at"=> "@",
+    "and"=> "&"
+  }
+end
+
+def word_substituter(tweet_str)
+  sub_ref = dictionary
+  tweet_arr = tweet_str.split
+  
+  tweet_arr.each do |word|
+    if sub_ref.key?(word.downcase)
+      tweet_arr[tweet_arr.index(word)] = sub_ref[word.downcase]
+    end
+  end
+  
+  tweet_arr.join(" ")
+end
+    
+def bulk_tweet_shortener(arr_of_tweets)
+  arr_of_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def 
+    
+    
+    
